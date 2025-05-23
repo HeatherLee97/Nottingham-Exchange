1342a3da9419e5894c2b5fb73d082f51313e4a63
myrandathompson <myrandathompson@icloud.com>
2025-05-22 19:22:01 -0700

    watchlist

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,17 +1,10 @@
-import { createBrowserRouter } from 'react-router-dom';
-import LoginFormPage from '../components/LoginFormPage';
-import SignupFormPage from '../components/SignupFormPage';
-import StockDetails from '../components/StockDetailsPage/StockDetails';
-import Layout from './Layout';
+import Dashboard from '../components/Dashboard'; // Import the Dashboard component
 
-export const router = createBrowserRouter([
+const router = createBrowserRouter([
   {
-    element: <Layout />,
+    path: "/",
+    element: <App />,
     children: [
-      {
-        path: "/",
-        element: <h1>Welcome!</h1>,
-      },
       {
         path: "login",
         element: <LoginFormPage />,
@@ -23,6 +16,10 @@ export const router = createBrowserRouter([
       {
         path: "stocks",
         element: <StockDetails />,
+      },
+      {
+        path: "dashboard", // Add the Dashboard route
+        element: <Dashboard />,
       },
     ],
   },
